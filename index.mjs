7209bac75fedfedacf727f1eeaacb2cb3a498253
ianschwartz <ianschwartz@cargurus.com>
2021-12-15 12:08:43 -0500

    new build 1639588123723

--- index.mjs
+++ index.mjs
@@ -2,15 +2,19 @@ import checkMessages from './checkMessages.mjs';
 import {Build} from "./models/Build.mjs";
 import child_process from "child_process";
 
-checkMessages().then(() => {
-    const b = new Build();
-    b.done.then(() => {
-        child_process.exec(`git add -A && git commit -m "new build ${Date.now()}" && git push`, (err, stdout, stderr) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log(stdout);
-        })
+const main = async () => {
+    await checkMessages();
+    await new Build().done;
+    child_process.exec(`git add -A && git commit -m "new build ${Date.now()}" && git push`, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(stdout);
     })
+
+}
+
+main().then(() => {
+    setInterval(main, 1200000)
 })
