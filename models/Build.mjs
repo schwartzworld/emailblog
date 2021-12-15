5633d9e16161eb193fa1957ae8a332aae43bfd74
ianschwartz <ianschwartz@cargurus.com>
2021-12-15 13:52:50 -0500

    new build 1639594370235

--- models/Build.mjs
+++ models/Build.mjs
@@ -23,13 +23,14 @@ export class Build {
 
     static createIndex = (msgs = []) => {
         const reversed = [...msgs].reverse();
-        const slugs = reversed.map(m => {
+        const [first, ...rest] = reversed;
+        const slugs = rest.map(m => {
             return `<li><a href="./build/${m.id}.html">${m.subject || "(no subject)"}</a><br/><small>${m.date.toLocaleString()}</small></li>`
         }).join('\n');
 
         const innerHTML = `
         <h1>Generated Email Blog</h1>
-        ${reversed[0].toHTML()}
+        ${first.toHTML()}
         <h3>Archive</h3>
         <ul>
             ${slugs}
@@ -53,6 +54,14 @@ export class Build {
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title>${title}</title>
 <link rel="stylesheet" href="/tufte.css">
+<style>
+    img {
+        max-width: 90%;
+    }
+    .preview {
+        max-width: 20rem;
+    }
+</style>
 </head>
 <body>
 ${html}
